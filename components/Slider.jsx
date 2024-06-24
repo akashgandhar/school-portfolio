c2d33ab83dfdae41684ea5d0d9242b32d15279de
akashgandhar <akashgandhar007@gmail.com>
2024-06-24 11:36:37 +0530

    initial

---
+++ components/Slider.jsx
@@ -0,0 +1,132 @@
+import React from 'react'
+
+export default function Slider() {
+  return (
+    <section class="creative-showcase--slider">
+  <div class="banner-horizental">
+    <div class="swiper swiper-container-h">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <div class="slide-bg " style="background-image: url(https://cdn.shopify.com/s/files/1/0617/2760/4809/files/s_1.jpg?v=1718998125);" data-swiper-parallax="1152">
+            <div class="slide-container">
+              <div class="slide-row">
+                <div class="slider-content">
+                  <h6 class="slide-subtitle" data-swiper-parallax="-1000"></h6>
+                  <h1 class="slide-heading" data-swiper-parallax="-2000"> <a href="https://www.fiverr.com/aliali44"><span></span></a> </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="slide-bg overla-dark" style="background-image: url(https://cdn.shopify.com/s/files/1/0617/2760/4809/files/s_2.jpg?v=1718998226);" data-swiper-parallax="1152">
+            <div class="slide-container">
+              <div class="slide-row">
+                <div class="slider-content">
+                  <h6 class="slide-subtitle" data-swiper-parallax="-1000"></h6>
+                   <h1 class="slide-heading"> 
+                    <a href="https://www.fiverr.com/aliali44">
+                      <span data-swiper-parallax="-2000">Earthmade</span>
+                      <br/>
+                      <span data-swiper-parallax="-3000">Aroma box</span>
+                    </a> </h1> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="slide-bg overla-light" style="background-image: url(https://cdn.shopify.com/s/files/1/0617/2760/4809/files/s_3.jpg?v=1718998231);" data-swiper-parallax="1152">
+            <div class="slide-container">
+              <div class="slide-row">
+                <div class="slider-content">
+                  <h6 class="slide-subtitle" data-swiper-parallax="-1000"></h6>
+                   <h1 class="slide-heading"> <a href="https://www.fiverr.com/aliali44">
+                      <span data-swiper-parallax="-2000">Access Bank</span>
+                      <br/>
+                      <span data-swiper-parallax="-3000">Rebranding</span>
+                    </a> </h1> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="slide-bg overla-dark" style="background-image: url(https://cdn.shopify.com/s/files/1/0617/2760/4809/files/s_4.jpg?v=1718998231);" data-swiper-parallax="1152">
+            <div class="slide-container">
+              <div class="slide-row">
+                <div class="slider-content">
+                  <h6 class="slide-subtitle" data-swiper-parallax="-1000"></h6>
+                   <h1 class="slide-heading" data-swiper-parallax="-2000"> <a href="https://www.fiverr.com/aliali44"><span>The joy of music</span></a> </h1> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="slide-bg overla-dark" style="background-image: url(https://cdn.shopify.com/s/files/1/0617/2760/4809/files/s_5.jpg?v=1718998232);" data-swiper-parallax="1152">
+            <div class="slide-container">
+              <div class="slide-row">
+                <div class="slider-content">
+                  <h6 class="slide-subtitle" data-swiper-parallax="-1000"></h6>
+                   <h1 class="slide-heading" data-swiper-parallax="-2000"> <a href="https://www.fiverr.com/aliali44"><span>Blue Adobe MAX</span></a> </h1> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="swiper-slide">
+          <div class="slide-bg overlay-dark" style="background-image: url(https://cdn.shopify.com/s/files/1/0617/2760/4809/files/s_6.jpg?v=1718998231);" data-swiper-parallax="1152">
+            <div class="slide-container">
+              <div class="slide-row">
+                <div class="slider-content">
+                  <h6 class="slide-subtitle" data-swiper-parallax="-1000"></h6>
+                   <h1 class="slide-heading" data-swiper-parallax="-2000"> <a href="https://www.fiverr.com/aliali44"><span>Carved Wood</span></a> </h1> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+         <div class="swiper-slide">
+          <div class="slide-bg overlay-dark" style="background-image: url(https://imgpanda.com/upload/ib/gCzPpwgzAw.png);" data-swiper-parallax="1152">
+            <div class="slide-container">
+              <div class="slide-row">
+                <div class="slider-content">
+                  <h6 class="slide-subtitle" data-swiper-parallax="-1000">© Creative Salahu</h6>
+                  <h1 class="slide-heading"> <a href="https://www.fiverr.com/aliali44">
+                      <span data-swiper-parallax="-2000">Abstract</span>
+                      <br/>
+                      <span data-swiper-parallax="-3000">Animation Video</span>
+                    </a> </h1>
+                </div>
+              </div>
+            </div>
+            <div class="video-container">
+              <video autoplay="" loop="" muted="" baba-icon="yes">
+                <source src="https://ui-themez.smartinnovates.net/items/infolio/Infolio/assets/imgs/works/full/vid.mp4" type="video/mp4"/>
+              </video>
+            </div>
+          </div>
+        </div> 
+      </div>
+      <div class="swiper-button-wrapper creative-button--wrapper">
+        <div class="swiper-button-next" tabindex="0" role="button" aria-label="Next slide">
+          <div>
+            <span>Next</span>
+          </div>
+          <div><i class="fas fa-chevron-right"></i></div>
+        </div>
+        <div class="swiper-button-prev" tabindex="0" role="button" aria-label="Previous slide">
+          <div><i class="fas fa-chevron-left"></i></div>
+          <div>
+            <span>Prev</span>
+          </div>
+        </div>
+      </div>
+      <div class="swiper-pagination">
+      </div>
+    </div>
+  </div>
+</section>
+  )
+}
