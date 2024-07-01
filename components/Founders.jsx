ff11880c5eba0d07cc3375eb31aa3335e0a25155
akashgandhar <akashgandhar007@gmail.com>
2024-07-01 11:46:21 +0530

    final

--- components/Founders.jsx
+++ components/Founders.jsx
@@ -14,7 +14,7 @@ export default function Founders() {
   return (
     <div className="px-8  w-full">
       <h2 class="text-3xl font-bold tracking-tight text-center sm:text-4xl mb-4">
-        Our Founders
+        Aim and Vision
       </h2>
       <Swiper
         pagination={{
@@ -28,18 +28,33 @@ export default function Founders() {
           disableOnInteraction: true,
         }}
       >
+         <SwiperSlide>
+          <div className="w-full h-full  rounded-xl flex">
+            <div className="w-full h-full flex flex-col p-8  items-start">
+              <h3 class="text-3xl font-bold tracking-tight text-center sm:text-4xl mb-4">
+                Founder&apos;s Message
+              </h3>
+              <p className="text-lg text-left">
+              &quot;Welcome to our visionary institution—a beacon of hope and transformation. Our retired senior officers, once stalwarts in the Armed Forces, lead this noble endeavor. In Betul&apos;s serene district, we nurture moral values, national pride, and holistic growth. Alongside academics, specialized courses prepare students for the Armed Forces. Our goal? To build a generation of integrity, resilience, and love for our nation.&quot;<br/><br/>- Founder, Ojas Sikshan and Prasikshan Sansthan
+              </p>
+            </div>
+            <div className="w-full p-8">
+              <div className="w-full rounded-xl h-full bg-center bg-no-repeat bg-contain bg-[url('/founders/anil.jpg')]"></div>
+            </div>
+          </div>{" "}
+        </SwiperSlide>
         <SwiperSlide>
           <div className="w-full h-full  rounded-xl flex">
             <div className="w-full h-full flex flex-col p-8  items-start">
               <h3 class="text-3xl font-bold tracking-tight text-center sm:text-4xl mb-4">
-                Founder 1
+                AIM
               </h3>
               <p className="text-lg text-left">
               The aim of the institution si ot train the students for primarily joining the Armed and Para Military Forces, However every student should eb able to select a career of his choice according ot his ability.
               </p>
             </div>
             <div className="w-full p-8">
-              <div className="w-full rounded-xl h-full bg-cover bg-no-repeat bg-[url('/slider/s1.jpg')]"></div>
+              <div className="w-full rounded-xl h-full bg-cover bg-no-repeat bg-[url('/gallery/17.jpeg')]"></div>
             </div>
           </div>
         </SwiperSlide>
@@ -47,32 +62,18 @@ export default function Founders() {
           <div className="w-full h-full  rounded-xl flex">
             <div className="w-full h-full flex flex-col p-8  items-start">
               <h3 class="text-3xl font-bold tracking-tight text-center sm:text-4xl mb-4">
-                Founder 1
+                VISION
               </h3>
               <p className="text-lg text-left">
               Vision of Ojas Sikshan and Prasikshan Sansthan is to create an educational institution of a very high standard, which will provide an ideal environment for hte under privileged youth of the region ot grow and nurture into fine citizens of the country, who have moral values, wisdom and love for the country.
               </p>
             </div>
             <div className="w-full p-8">
-              <div className="w-full rounded-xl h-full bg-cover bg-no-repeat bg-[url('/slider/s1.jpg')]"></div>
+              <div className="w-full rounded-xl h-full bg-cover bg-no-repeat bg-[url('/gallery/19.jpeg')]"></div>
             </div>
           </div>{" "}
         </SwiperSlide>
-        <SwiperSlide>
-          <div className="w-full h-full  rounded-xl flex">
-            <div className="w-full h-full flex flex-col p-8  items-start">
-              <h3 class="text-3xl font-bold tracking-tight text-center sm:text-4xl mb-4">
-                Founder 1
-              </h3>
-              <p className="text-lg text-left">
-              &quot;Welcome to our visionary institution—a beacon of hope and transformation. Our retired senior officers, once stalwarts in the Armed Forces, lead this noble endeavor. In Betul&apso;s serene district, we nurture moral values, national pride, and holistic growth. Alongside academics, specialized courses prepare students for the Armed Forces. Our goal? To build a generation of integrity, resilience, and love for our nation.&quot;
-              </p>
-            </div>
-            <div className="w-full p-8">
-              <div className="w-full rounded-xl h-full bg-cover bg-no-repeat bg-[url('/slider/s1.jpg')]"></div>
-            </div>
-          </div>{" "}
-        </SwiperSlide>
+       
         
       </Swiper>
     </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
 //ojas shikshan and pariksha sansthan
 import React, { useEffect, useState } from "react";
@@ -29,16 +30,29 @@ const Navbar = () => {
   return (
     <header
       className={`${
-        scrolled ? "bg-[#255c39] text-white transition-all duration-300 ease-in-out" : "bg-tranparent text- transition-all duration-300 ease-in-out"
+        scrolled
+          ? "bg-[#255c39] text-white transition-all duration-300 ease-in-out"
+          : "bg-tranparent text- transition-all duration-300 ease-in-out"
       } lg:px-16 px-4  w-full flex flex-wrap items-center border-b font-black py-3 fixed bg-cover bg-no-repeat top-0 z-[999]`}
     >
-      <div className="flex-1 flex justify-between items-center">
-        <Link href="/" className="text-xl uppercase">
-        ojas shikshan and pariksha sansthan 
+      <Link href="/" className="text-xl uppercase">
+        <div className="flex-1 flex justify-between items-center">
+          <Image
+            width={1920}
+            height={1080}
+            src="/logo.png"
+            alt="Logo"
+            className="mr-2 w-12 h-12"
+          />
+        </div>
+      </Link>
+      <div className="flex-1 flex justify-between items-center sm">
+        <Link href="/" className="text-xl hidden sm:block uppercase">
+          ojas shikshan and pariksha sansthan
         </Link>
       </div>
 
-      <div className="pointer-cursor md:hidden block" onClick={toggleMenu}>
+      <div className="cursor-pointer sm:hidden block" onClick={toggleMenu}>
         <svg
           className="fill-current "
           xmlns="http://www.w3.org/2000/svg"
@@ -59,7 +73,11 @@ const Navbar = () => {
       >
         <nav>
           <ul className="md:flex items-center justify-between text-base  pt-4 md:pt-0">
-            
+            <li>
+              <Link className="md:p-4 py-3 px-0 block" href="/">
+                Home
+              </Link>
+            </li>
             <li>
               <Link className="md:p-4 py-3 px-0 block" href="/founders">
                 Founders
@@ -76,7 +94,10 @@ const Navbar = () => {
               </Link>
             </li>
             <li>
-              <Link className="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="/contactus">
+              <Link
+                className="md:p-4 py-3 px-0 block md:mb-0 mb-2"
+                href="/contactus"
+              >
                 Contact Us
               </Link>
             </li>
